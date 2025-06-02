c237758aa484fd1dacc17658f7f20cc14b6aca01
sujita <sujitaregmi189@gmail.com>
2025-06-02 10:32:39 +0545

    day 9

---
+++ demo/readme.md
@@ -0,0 +1,4 @@
+This is a demo project created for learning and demonstration purposes only.
+It showcases basic features and functionality, but it's not a complete or production-ready application.
+Some data, content, or components may be placeholder or incomplete.
+Feel free to explore and test, but keep in mind this is still under development.
---
+++ demo/word/models.py
@@ -0,0 +1,25 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class BaseTimestampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Post(BaseTimestampModel):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+def _str_(self):
+    return self.title
+
+class Comment(BaseTimestampModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name = "comments")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "author")
+    content = models.TextField()
+
+def _str_(self):
+    return f"{self.post.title}"
