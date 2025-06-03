2f764d7030322d7c2aae20170a727df68766e3ec
sujita <sujitaregmi189@gmail.com>
2025-06-03 11:02:19 +0545

    .

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_pme1jepgokqo5kk!vnnwzlo1xbm^el%(efbk)@x3*z3lbzka^'
+SECRET_KEY = config('SECRET_KEY', 
+default = "defaultkey@123")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
